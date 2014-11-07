69d7be4f05e838123a70e7c3725e601f53d936aa
Sergey Shandar <serge.shandar@gmail.com>
2014-11-06 17:43:33 -0800

    A missed file.

---
+++ Framework/Framework/Optional.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Framework
+{
+    public abstract class Optional<T>
+    {
+        public abstract TR Select<TR>(Func<T, TR> hasValue, Func<TR> hasNoValue);
+
+        public void Select(Action<T> hasValue, Action hasNoValue)
+        {
+            Select(
+                value =>
+                {
+                    hasValue(value);
+                    return new Void();
+                },
+                () =>
+                {
+                    hasNoValue();
+                    return new Void();
+                });
+        }
+
+        public void Select(Action<T> hasValue)
+        {
+            Select(hasValue, () => {});
+        }
+
+        public IEnumerable<T> ToEnumerable()
+        {
+            return Select(value => value.Enumerate(), Enumerable.Empty<T>);
+        }
+
+        public static Optional<T> CreateNoValue()
+        {
+            return new NoValue();
+        }
+
+        public sealed class NoValue: Optional<T>
+        {
+            public override TR Select<TR>(Func<T, TR> hasValue, Func<TR> hasNoValue)
+            {
+                return hasNoValue();
+            }
+        }
+
+        public sealed class Value : Optional<T>
+        {
+            public Value(T value)
+            {
+                _value = value;
+            }
+
+            public override TR Select<TR>(Func<T, TR> hasValue, Func<TR> hasNoValue)
+            {
+                return hasValue(_value);
+            }
+
+            private readonly T _value;
+        }
+    }
+}
--- Framework/Framework/OptionalClass.cs
+++ Framework/Framework/OptionalClass.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Framework
+﻿namespace Framework
 {
     public struct OptionalClass<T>
         where T: class
