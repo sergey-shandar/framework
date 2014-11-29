d587425ddfee680d61fb57ec08436f212f79a27b
Sergey Shandar <serge.shandar@gmail.com>
2014-11-29 13:37:04 -0800

    T4

---
+++ Framework/Framework/File.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml;
+using System;
+
+namespace Framework
+{
+    public static class File
+    {
+        public static void Write(string fileName, Action<XmlWriter> action)
+        {
+        }
+    }
+}
--- Framework/Framework/Properties/AssemblyInfo.cs
+++ Framework/Framework/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Framework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# Framework")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Framework")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Sergey Shandar 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
