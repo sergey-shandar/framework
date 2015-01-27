037614ed233fede1f19c1afc4cbff82b31f25b9e
Sergey Shandar <serge.shandar@gmail.com>
2015-01-26 20:02:15 -0800

    Cli stub

---
+++ Framework.G1/Framework.G1/Cli.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Framework.G1
+{
+    public static class Cli
+    {
+        public static int Run<T>(string[] args)
+        {
+            var type = typeof(T);
+            var methods = type.GetMethods();
+            foreach(var method in methods)
+            {
+                
+            }
+            return 0;
+        }
+    }
+}
